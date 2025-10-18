672a5e2327411aaf80da2015fead3097499e5853
Hunter W <hjwebb9@gmail.com>
2025-10-18 18:48:47 -0400

    remove unused import

--- valorant-stat-visualizer/app/page.tsx
+++ valorant-stat-visualizer/app/page.tsx
@@ -1,5 +1,4 @@
 import Image from "next/image";
-import DatabaseTest from "@/components/DatabaseTest";
 import PlayerStatsDisplay from "@/components/PlayerStatsDisplay";
 
 export default function Home() {
